57d524c013a64bdf071770818abc4c0706af0b74
王佳 <wangjia_1919@163.com>
2019-05-10 11:51:55 +0800

    add getaway

---
+++ src/main/java/com/springcloudify/cloudify/eurekaserver/EurekaGetawayApplication.java
@@ -0,0 +1,12 @@
+package com.springcloudify.cloudify.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class EurekaGetawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaGetawayApplication.class, args);
+    }
+}
---
+++ src/main/java/com/springcloudify/cloudify/eurekaserver/EurekaGetawayConfig.java
@@ -0,0 +1,13 @@
+package com.springcloudify.cloudify.eurekaserver;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Create by Jarvis.wang on Jarvis's device
+ *
+ * @author Jarvis.wang  Jarvis
+ * @date 2019-05-10 11:30
+ */
+@EnableZuulProxy
+public class EurekaGetawayConfig {
+}
